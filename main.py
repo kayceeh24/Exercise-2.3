36f3f15c8da6db76d7c83efe82aa88f6e875598e
KC Chester Chester <113367258+kayceeh24@users.noreply.github.com>
2022-10-27 01:27:52 +0000

    output from 31 to 19

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ recordings = int(recordings)
 if page_views < 0 or snap_shots < 0 or recordings < 0:
     print("These values can't be negative")
 elif page_views < 500000 or snap_shots < 100 or recordings < 5000:
-    print("You should subscribe to our ENTERPRISE tier")
-    print("Contact us for a quote")
+    print("You should subscribe to our BASIC tier")
+    print("The cost is $24 per month")
 elif page_views > 150000 or snap_shots > 75 or recordings > 1000:
     print("You should subscribe to our PRO tier")
     print("The cost is $249 per month")
@@ -28,5 +28,5 @@ elif page_views < 30000 or snap_shots > 25 or recordings > 100:
     print("You should subscribe to our STANDARD tier")
     print("The cost is $49 per month")
 else:
-    print("You should subscribe to our BASIC tier")
-    print("The cost is $24 per month")
+    print("You should subscribe to our ENTERPRISE tier")
+    print("Contact us for a quote")
